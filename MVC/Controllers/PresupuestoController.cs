a405e0d1d96c3c32d31476ea148b4b319d1c3933
juancookz <jcavila383@gmail.com>
2024-11-12 20:23:53 -0300

    Listar Presupuestos

---
+++ MVC/Controllers/PresupuestoController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers;
+
+public class PresupuestoController : Controller
+{
+    private readonly PresupuestoRepository presupuestoRepository;
+    public PresupuestoController()
+    {
+        presupuestoRepository = new PresupuestoRepository(@"Data Source=db/Tienda.db;Cache=Shared");
+    }
+    public IActionResult Listar()
+    {
+        List<Presupuesto> ListaPresup = presupuestoRepository.GetAll();
+        return View(ListaPresup);
+    }
+}
