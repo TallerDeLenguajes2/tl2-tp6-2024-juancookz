9052175a2e1c35a8324f32cccce03ecb0746db01
juancookz <jcavila383@gmail.com>
2024-11-17 00:45:01 -0300

    Listar clientes

--- MVC/Controllers/ClienteController.cs
+++ MVC/Controllers/ClienteController.cs
@@ -1,5 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 public class ClienteController : Controller
 {
+    private readonly ClienteRepository clienteRepository;
+    public ClienteController()
+    {
+        clienteRepository = new ClienteRepository(@"Data Source=db/Tienda.db;Cache=Shared");
+    }
+    public IActionResult Listar()
+    {
+        var clientes = clienteRepository.GetAll();
+        return View(clientes);
+    }
     
 }
